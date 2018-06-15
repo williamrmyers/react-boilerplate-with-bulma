18b0249a3b4e855dfdb59985a8eeaca98b4eb96f
William Myers <williamrmyers@gmail.com>
2018-06-15 00:10:30 -0700

    Added Bulma Nav bar, and converted JS menu to React.

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,61 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 
-const Header = () => (
-  <div>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/me">me</NavLink>
-  </div>
-);
+class Header extends React.Component {
+  state = {
+    hambergerToggle: ""
+  };
 
+  handleLogin = () => {
+  console.log(`Login Button Pushed.`);
+  }
+
+  toggleBerger = (state) => {
+    console.log(`Hamberger clicked.`);
+        if (this.state.hambergerToggle){
+              this.setState(() => ({hambergerToggle: ""}))
+            } else {
+              this.setState(() => ({hambergerToggle: 'is-active' }))
+        }
+    }
+
+  render() {
+
+    // const myClassName = ' is-active ';
+    let logginButton = 'Sign Up';
+
+    return (
+      <div>
+        <section className="section">
+        <nav className="navbar is-black is-fixed-top">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="index.html">
+              <img src="acme.png" alt="A boilerplate App" width="112" height="28"/>
+            </a>
+            <div className="navbar-burger burger" onClick={this.toggleBerger} data-target="navbarExampleTransparentExample">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div id="navbarExampleTransparentExample" className={this.state.hambergerToggle + " navbar-menu"} >
+            <div className="navbar-start"></div>
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="field is-grouped">
+                  <p className="control">
+                    <a className="button Normal is-outlined" onClick={this.handleLogin}>
+                      <span>{logginButton}</span>
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </section>
+      </div>
+    );
+  }
+}
 export default Header;
