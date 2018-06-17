62aa6e535903abf223a4b5ece621cd6d74d70cee
William Myers <williamrmyers@gmail.com>
2018-06-17 15:05:40 -0700

    Updated /home route.

--- src/components/home.js
+++ src/components/home.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    <h1>Welcome</h1>
-    <p>This is the home Page.</p>
+    <section className="section">
+      <div className="hero">
+        <div className="hero-body">
+          <div className="container has-text-centered content">
+              <h1 className="big-header">Welcome!</h1>
+              <img src="https://media.giphy.com/media/CY9jl58dVtU2s/giphy.gif" alt="Cute Kitten"/>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
